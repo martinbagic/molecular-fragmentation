c347a908afc2ef7305f305b54339f4e90ac418b2
Martin Bagic <martin.bagic@outlook.com>
2020-02-20 17:34:23 +0100

    add bash cmd functions

--- hasse/hack.py
+++ hasse/hack.py
@@ -1,11 +1,9 @@
-# run from cheminf
-
 import os
 import subprocess
+import helper
 
 
 def get_mcs(smiles1, smiles2):
-    mcs = "./mcs-cliquer-1.0.0/mcs/mcs"
 
     with open('temp1.smi', 'w') as f:
         f.write(smiles1)
@@ -13,7 +11,7 @@ def get_mcs(smiles1, smiles2):
     with open('temp2.smi', 'w') as f:
         f.write(smiles2)
 
-    args = [mcs, 'temp1.smi', 'temp2.smi']
+    args = [helper.MCS_PATH, 'temp1.smi', 'temp2.smi']
     r = subprocess.run(args, stdout=subprocess.PIPE)
 
     stdout = r.stdout.decode('utf8')
@@ -28,18 +26,9 @@ def get_mcs(smiles1, smiles2):
 
     return smiles3
 
-# if __name__ == '__main__':
-#     mol1 = "O1[C@@H]2[C@]34[C@H]([C@H](N(CC3)C)Cc3c4c1c(O)cc3)C=C[C@@H]2O"
-#     mol2 = "O[C@H](c1cc(O)c(O)cc1)CNC"
-#     get_mcs(mol1,mol2)
 
-
-# - obabel -:"C12C(CCC1)CCCC2" -o svg > mol.svg
-
-def make_mol(smiles, path):
-    # print(smiles)
-    s = f'obabel -:"{smiles}" -o svg > {path}'
-    # args = s.split()
+def draw_smiles(smiles, path):
+    s = f'obabel -:"{smiles}" -o svg > "{path}"'
     subprocess.run(s, shell=True)
 
 
@@ -50,6 +39,5 @@ def get_canonical(smiles, canonics):
         r = subprocess.run(s, stdout=subprocess.PIPE, shell=True)
         stdout = r.stdout.decode('utf8')
         canonical_smiles = stdout.strip()
-        canonical_smiles = canonical_smiles.replace('/','').replace('\\','')
+        canonical_smiles = canonical_smiles.replace('/', '').replace('\\', '')
         canonics[smiles] = canonical_smiles
-
