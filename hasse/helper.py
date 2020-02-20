1750dc97a33379d61bd4fc4739bf61bb53cb5719
Martin Bagic <martin.bagic@outlook.com>
2020-02-20 17:33:56 +0100

    add helper functions

---
+++ hasse/helper.py
@@ -0,0 +1,22 @@
+import os
+
+
+def PATH(s):
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    if isinstance(s, str):
+        return os.path.join(base_dir, s)
+    else:
+        return os.path.join(base_dir, *s)
+
+MCS_PATH = PATH(['..','mcs-cliquer-1.0.0','mcs','mcs'])
+
+DIGRAPH_SETTINGS = {
+    'edge_attr': {
+        'arrowhead': 'vee',
+        'arrowsize': '0.5',
+        'splines': 'curved',
+    },
+    'format': 'svg',
+
+}
+
