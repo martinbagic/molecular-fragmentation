5d9691ae199bd4ba5fcef05e85b8b5c6ae914bd1
Martin Bagic <martin.bagic@outlook.com>
2020-02-27 00:43:48 +0100

    remove temp files

--- hasse/funcs/hack.py
+++ hasse/funcs/hack.py
@@ -7,24 +7,18 @@ import logging
 def get_mcs(smiles1, smiles2):
     ''' Run mcs-cliquer over cmd. '''
 
-    with open('temp1.smi', 'w') as f:
-        f.write(smiles1)
 
-    with open('temp2.smi', 'w') as f:
-        f.write(smiles2)
-
-    args = [helper.MCS_PATH, 'temp1.smi', 'temp2.smi']
+    args = [helper.MCS_PATH, smiles1, smiles2]
     r = subprocess.run(args, stdout=subprocess.PIPE)
 
     stdout = r.stdout.decode('utf8')
-    try:
-        smiles3 = stdout.split()[-2]
-    except:
-        logging.error(f'no MCS for "{smiles1}" and "{smiles2}"')
-        smiles3 = ''
 
-    os.remove('temp1.smi')
-    os.remove('temp2.smi')
+    smiles3 = []
+    for line in stdout.split('\n'):
+        if line.startswith('rEsUlT'):
+            mol = line.split()[1].strip()
+            smiles3.append(mol)
+
 
     return smiles3
 
@@ -37,6 +31,7 @@ def draw_smiles(smiles, path):
 class Canonicalizer:
     def __init__(self):
         self.pairs = dict()
+        self.splittings = dict()
 
     def __call__(self, smiles):
         if smiles not in self.pairs:
@@ -49,16 +44,15 @@ class Canonicalizer:
             canonical_smiles = canonical_smiles.replace(
                 '/', '').replace('\\', '').replace('@', '')
 
-            biggest_smiles = self.biggest(canonical_smiles)
-            if canonical_smiles != biggest_smiles:
-                logging.debug(f'splitting "{canonical_smiles}" into "{biggest_smiles}"')
-
-            self.pairs[smiles] = biggest_smiles
+            # canonical_smiles = self.biggest(canonical_smiles)
+            self.pairs[smiles] = canonical_smiles
 
         return self.pairs[smiles]
 
     def biggest(self, smiles):
-        return max(
-            smiles.split('.'),
-            key=lambda s: sum(c.isalpha() for c in s)
-        )
+        if smiles not in self.splittings:
+            self.splittings[smiles] = max(
+                smiles.split('.'),
+                key=lambda s: sum(c.isalpha() for c in s)
+            )
+        return self.splittings[smiles]
